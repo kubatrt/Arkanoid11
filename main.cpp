0cc29acf23b45897b93d0de10a6fff284ae374ec
Kuba <kubatrt@gmail.com>
2016-12-24 04:00:36 +0100

    Three brick colors.

--- main.cpp
+++ main.cpp
@@ -2,161 +2,10 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "config.h"
+#include "structures.hpp"
+#include "collision.hpp"
 
-
-const char * appName = "Arkanoid11";
-constexpr int windowWidth{800}, windowHeight{600};
-
-constexpr float ballRadius{10.0f}, ballVelocity{8.0f};
-constexpr float paddleWidth{96.0f}, paddleHeight{24.0f}, paddleVelocity {6.0f};
-constexpr float brickWidth{64.0f}, brickHeight{24.0f};
-constexpr float countBlocksX{10}, countBlocksY{4};
-
-struct Ball
-{
-  sf::CircleShape shape;
-  sf::Vector2f    velocity{-ballVelocity, -ballVelocity};
-
-  Ball(float mX, float mY)
-  {
-    shape.setPosition(mX, mY);
-    shape.setRadius(ballRadius);
-    shape.setFillColor(sf::Color::Red);
-    shape.setOrigin(ballRadius, ballRadius);
-  }
-
-  void checkBoundaries()
-  {
-    if(left() < 0) velocity.x = ballVelocity;
-    else if(right() > windowWidth) velocity.x = -ballVelocity;
-
-    else if(top() < 0) velocity.y = ballVelocity;
-    else if(bottom() > windowHeight) velocity.y = -ballVelocity;
-  }
-
-  void update()
-  {
-    shape.move(velocity);
-    checkBoundaries();
-  }
-
-  // "property" methods
-  float x()       { return shape.getPosition().x; }
-  float y()       { return shape.getPosition().y; }
-  float left()    { return x() - shape.getRadius(); }
-  float right()   { return x() + shape.getRadius(); }
-  float top()     { return y() - shape.getRadius(); }
-  float bottom()  { return y() + shape.getRadius(); }
-};
-
-
-struct Paddle
-{
-  sf::RectangleShape  shape;
-  sf::Vector2f        velocity;
-
-  Paddle(float mX, float mY)
-  {
-    shape.setPosition(mX, mY);
-    shape.setSize({paddleWidth, paddleHeight});
-    shape.setFillColor(sf::Color::Green);
-    shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
-  }
-
-  void update()
-  {
-    shape.move(velocity);
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && left() > 0)
-      velocity.x = -paddleVelocity;
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && right() < windowWidth)
-      velocity.x = paddleVelocity;
-    else
-      velocity.x = 0;
-  }
-
-  // "property" methods
-  float x()       { return shape.getPosition().x; }
-  float y()       { return shape.getPosition().y; }
-  float left()    { return x() - shape.getSize().x / 2.f; }
-  float right()   { return x() + shape.getSize().x / 2.f; }
-  float top()     { return y() - shape.getSize().y / 2.f; }
-  float bottom()  { return y() + shape.getSize().y / 2.f; }
-};
-
-struct Brick
-{
-  sf::RectangleShape  shape;
-
-  bool destroyed{false};
-
-  Brick(float mX, float mY)
-  {
-    shape.setPosition(mX, mY);
-    shape.setSize({brickWidth, brickHeight});
-    shape.setFillColor(sf::Color::Yellow);
-    shape.setOrigin(brickWidth / 2.f, brickHeight / 2.f);
-  }
-
-  void update()
-  {
-
-  }
-
-  // "property" methods
-  float x()       { return shape.getPosition().x; }
-  float y()       { return shape.getPosition().y; }
-  float left()    { return x() - shape.getSize().x / 2.f; }
-  float right()   { return x() + shape.getSize().x / 2.f; }
-  float top()     { return y() - shape.getSize().y / 2.f; }
-  float bottom()  { return y() + shape.getSize().y / 2.f; }
-};
-
-
-template<class T1, class T2> bool isIntersecting(T1& mA, T2& mB)
-{
-  return  mA.right() >= mB.left() && mA.left() <= mB.right()
-       && mA.top() <= mB.bottom() && mA.bottom() >= mB.top();
-}
-
-void testCollision(Paddle& mPaddle, Ball& mBall)
-{
-  // if no collision get out from the function
-  if(!isIntersecting(mPaddle, mBall)) return;
-
-  mBall.velocity.y = -ballVelocity;
-
-  // direct ball depends where's paddle was hit
-  if(mBall.x() < mPaddle.x())
-    mBall.velocity.x = -ballVelocity;
-  else
-    mBall.velocity.x = ballVelocity;
-}
-
-void testCollision(Brick& mBrick, Ball& mBall)
-{
-  if(!isIntersecting(mBrick, mBall)) return;
-
-  mBrick.destroyed = true;
-
-  float overlapLeft{mBall.right() - mBrick.left()};
-  float overlapRight{mBrick.right() - mBall.left()};
-  float overlapTop{mBall.bottom() - mBall.top()};
-  float overlapBottom{mBrick.bottom() - mBall.top()};
-
-  bool ballFromLeft(abs(overlapLeft) < abs(overlapRight));
-  bool ballFromTop(abs(overlapTop) < abs(overlapBottom));
-
-  float minOverlapX{ballFromLeft ? overlapLeft : overlapRight};
-  float minOverlapY{ballFromTop ? overlapTop : overlapBottom};
-
-  // upon assumptions
-  if(abs(minOverlapX) < abs(minOverlapY))
-    mBall.velocity.x = ballFromLeft ? -ballVelocity : ballVelocity;
-  else
-    mBall.velocity.y = ballFromTop ? -ballVelocity : ballVelocity;
-}
-
+sf::Color brickColors[] = {sf::Color::Red, sf::Color::Green, sf::Color::Blue};
 
 int main()
 {
@@ -172,7 +21,8 @@ int main()
   for(int ix{0}; ix < countBlocksX; ++ix)
     for(int iy{0}; iy < countBlocksY; ++iy)
       bricks.emplace_back( (ix + 1) * (brickWidth + 4) + 20,
-                           (iy + 1) * (brickHeight + 4));
+                           (iy + 1) * (brickHeight + 4),
+                           brickColors[iy / 2]);
 
   sf::VideoMode vmode(windowWidth, windowHeight);
   sf::RenderWindow window(vmode, appName);
@@ -209,5 +59,6 @@ int main()
 
     window.display();
   }
+
   return 0;
 }
